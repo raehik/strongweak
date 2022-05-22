c94e122c8d50e1062669e950d9c81fc375aa2044
Ben Orchard <thefirstmuffinman@gmail.com>
2022-05-22 22:55:15 +0100

    add restrengthen

--- src/Strongweak.hs
+++ src/Strongweak.hs
@@ -1,9 +1,25 @@
 module Strongweak
   ( module Strongweak.Weaken
-  , module Strongweak.Strengthen
+  , module Strongweak.Strengthen, restrengthen
   , module Strongweak.SW
   ) where
 
 import Strongweak.Weaken
 import Strongweak.Strengthen
 import Strongweak.SW
+
+import Data.Validation
+import Data.List.NonEmpty
+
+-- | Weaken and re-strengthen a strong value.
+--
+-- In correct operation, @restrengthen === Right@. If your value was
+-- strengthened incorrectly, or perhaps you cheated via @UnsafeStrengthen@, this
+-- may not be the case. For example:
+--
+-- >>> restrengthen $ unsafeStrengthen' @(Vector 2 Natural) [0]
+-- Failure ...
+restrengthen
+    :: forall w s. (Weaken s w, Strengthen w s)
+    => s -> Validation (NonEmpty StrengthenError) s
+restrengthen = strengthen . weaken
