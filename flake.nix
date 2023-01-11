bbe09d8995bc3e7be3f7de84b984855cd968de6f
Ben Orchard <thefirstmuffinman@gmail.com>
2023-01-11 19:09:15 +0000

    add flake
    Mainly for dev env.

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    #nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    #flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
+      perSystem = { self', pkgs, ... }: {
+        haskellProjects.default = {
+          #haskellPackages = pkgs.haskell.packages.ghc925;
+          packages = {
+            strongweak.root = ./.;
+          };
+          # buildTools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
+          # overrides = self: super: { }
+          # hlintCheck.enable = true;
+          # hlsCheck.enable = true;
+        };
+      };
+    };
+}
