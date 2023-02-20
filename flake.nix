05030a5a694ad8e46ca29d81ea568cbcff8b50c2
Ben Orchard <thefirstmuffinman@gmail.com>
2023-02-20 00:09:37 +0000

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,26 @@
 {
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    #flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
   };
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
       perSystem = { self', pkgs, ... }: {
         haskellProjects.default = {
-          #haskellPackages = pkgs.haskell.packages.ghc925;
-          packages = {
-            strongweak.root = ./.;
-          };
-          # buildTools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
-          # overrides = self: super: { }
-          # hlintCheck.enable = true;
-          # hlsCheck.enable = true;
+          basePackages = pkgs.haskell.packages.ghc94;
+          # packages.example.root = ./.;  # This value is detected based on .cabal files
+          # overrides = self: super: { };
+          # devShell = {
+          #  enable = true;  # Enabled by default
+          #  tools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
+          #  hlsCheck.enable = true;
+          # };
         };
+        # haskell-flake doesn't set the default package, but you can do it here.
+        packages.default = self'.packages.strongweak;
       };
     };
 }
