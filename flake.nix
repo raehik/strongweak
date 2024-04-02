c842b2cca53a51f08877fc14ceacd5ebf6da7a4e
Ben Orchard <thefirstmuffinman@gmail.com>
2024-04-03 00:10:18 +0100

    redo nix flake

--- flake.nix
+++ flake.nix
@@ -3,38 +3,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
-    refined1 = {
-      url = "github:raehik/refined/refined1-hackage";
-      flake = false;
-    };
+    refined1.url = "github:raehik/refined/refined1-hackage";
+    refined1.flake = false;
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
-
       perSystem = { self', pkgs, config, ... }: {
-
-        haskellProjects.ghc96 = import ./haskell-flake-ghc96.nix pkgs;
-
-        haskellProjects.default = {
-          basePackages = config.haskellProjects.ghc96.outputs.finalPackages;
-
-          packages = {
-            refined1.source = inputs.refined1;
-          };
-
-          devShell = {
-            tools = hp: {
-              ghcid = null; # broken on GHC 9.6? old fsnotify
-              hlint = null; # broken on GHC 9.6? old
-              haskell-language-server = null; # TAKES AGES TO BUILD FFS
-            };
-          };
-
+        packages.default  = self'.packages.ghc96-strongweak;
+        devShells.default = self'.devShells.ghc96;
+        haskellProjects.ghc98 = {
+          basePackages = pkgs.haskell.packages.ghc98;
+          packages.refined1.source = inputs.refined1;
         };
-
-        packages.default = self'.packages.strongweak;
+        haskellProjects.ghc96 = {
+          basePackages = pkgs.haskell.packages.ghc96;
+          packages.refined1.source = inputs.refined1;
+          devShell.mkShellArgs.name = "ghc96-strongweak";
+        };
       };
     };
 }
