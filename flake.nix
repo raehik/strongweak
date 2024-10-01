74496ba01af8de3933b5fd09e39dd1084defc7ab
Ben Orchard <thefirstmuffinman@gmail.com>
2024-10-01 16:43:28 +0100

    update Nix flake

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
+    rerefined.url   = "github:raehik/rerefined";
+    rerefined.flake = false;
   };
   outputs = inputs:
   let
@@ -24,18 +26,27 @@
         devShells.default = self'.devShells.ghc98;
         haskellProjects.ghc910 = {
           basePackages = pkgs.haskell.packages.ghc910;
+          devShell = defDevShell "ghc910";
           packages.finite-typelits.source = "0.2.1.0";
           packages.vector-sized.source = "1.6.1";
           settings.defun-core.jailbreak = true;
-          devShell = defDevShell "ghc910";
+
+          # waiting on nixpkgs
+          packages.rerefined.source = inputs.rerefined;
         };
         haskellProjects.ghc98 = {
           basePackages = pkgs.haskell.packages.ghc98;
           devShell = defDevShell "ghc98";
+
+          # waiting on nixpkgs
+          packages.rerefined.source = inputs.rerefined;
         };
         haskellProjects.ghc96 = {
           basePackages = pkgs.haskell.packages.ghc96;
           devShell = defDevShell "ghc96";
+
+          # waiting on nixpkgs
+          packages.rerefined.source = inputs.rerefined;
         };
       };
     };
