a4bc7574f50fdbe07fc54387a9a431fad9ad8446
Ben Orchard <thefirstmuffinman@gmail.com>
2024-10-16 22:28:07 +0100

    delete Coercibly; cleanup
    Confirmed that SWChain works and is shorter than the Coercibly
    invocation it replaces.

--- src/Strongweak/Util/TypeErrors.hs
+++
@@ -1,9 +0,0 @@
-module Strongweak.Util.TypeErrors where
-
-import GHC.TypeError
-
-type ErrZeroInvariantNewtype a =
-       Text a :<>: Text " is a zero-invariant, coercible newtype"
-  :$$: Text "These may not be used with strongweak type classes directly"
-  :$$: Text "  due to ambiguity with their handling"
-  :$$: Text "Wrap it using Strongweak.Coercibly"
